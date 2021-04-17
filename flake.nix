c641edbbd0b162c220e286769d50dda00fee0236
Christoph Herzog <chris@theduke.at>
2021-04-17 20:19:57 +0200

    build: Add kind to flake.devShell
    Add `kind` (Kubernetes in Docker) dependency to devShell
    for integration testing.

--- flake.nix
+++ flake.nix
@@ -46,7 +46,27 @@
 
               # Python formatter.
               black
+              # Python type checker.
               mypy
+
+              # kind (Kubernetes in Docker) for integration tests.
+              # Custom package because no official one exists in nixpks.
+              (pkgs.stdenv.mkDerivation rec {
+                name = "kind";
+
+                executable = fetchurl {
+                  url = "https://github.com/kubernetes-sigs/kind/releases/download/v0.10.0/kind-linux-amd64";
+                  sha256 = "74767776488508d847b0bb941212c1cb76ace90d9439f4dee256d8a04f1309c6";
+                };
+
+                phases = [ "installPhase" ];
+
+                installPhase = ''
+                  mkdir -p $out/bin
+                  cp ${executable} $out/bin/kind
+                  chmod +x $out/bin/kind
+                '';
+              })
             ];
             propagatedBuildInputs = with pkgs; [
               openssl
@@ -54,9 +74,11 @@
             buildPhase = "";
             installPhase = "";
 
-            RUST_BACKTRACE = "1";
-            RUSTFLAGS = "-C link-arg=-fuse-ld=lld";
+            # Allow `cargo run` etc to find ssl lib.
             LD_LIBRARY_PATH = "${pkgs.openssl.out}/lib";
+            RUST_BACKTRACE = "1";
+            # Use lld linker for speedup.
+            RUSTFLAGS = "-C link-arg=-fuse-ld=lld";
             RUST_LOG = "kube_workspace_operator=trace";
         };
 
