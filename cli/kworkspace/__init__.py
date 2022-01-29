a0440f5cecb5d12c99494f4a55d46cf583c85c8c
ahirner <a.hirner@gmail.com>
2022-01-29 12:17:58 +0100

    replace removesuffix to run with python < 3.9

--- cli/kworkspace/__init__.py
+++ cli/kworkspace/__init__.py
@@ -530,7 +530,7 @@ def run() -> None:
     ssh_public_key = ""
     if not ssh_key_path:
         ssh_key_path = os.path.expanduser("~/.ssh/id_rsa.pub")
-    ssh_private_key_path = ssh_key_path.removesuffix(".pub")
+    ssh_private_key_path = ssh_key_path.rsplit(".pub", 1)[0]
 
     if os.path.isfile(ssh_key_path):
         with open(ssh_key_path) as keyfile:
